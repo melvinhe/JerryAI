12a92bc7637a9caa72a195f56bce0e36c5382e27
joe <joseph.white.bsa@gmail.com>
2023-01-29 05:15:58 -0600

    start on legislator card

---
+++ packages/frontend/src/components/LegislatorCard.tsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  StackDivider,
+  useDisclosure,
+  Text,
+  Spinner,
+  Flex,
+  Spacer,
+  Center,
+  ButtonGroup,
+  IconButton,
+  Tag,
+  Avatar,
+  CardFooter,
+  Image,
+  Container,
+} from "@chakra-ui/react";
+
+export function LegislatorCard() {
+  return (
+    <>
+      <Card maxW="md">
+        <CardHeader>
+          <Flex>
+            <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+              <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+
+              <Box>
+                <Heading size="sm">Segun Adebayo</Heading>
+                <Text>Creator, Chakra UI</Text>
+              </Box>
+            </Flex>
+            <IconButton
+              variant="ghost"
+              colorScheme="gray"
+              aria-label="See menu"
+            />
+          </Flex>
+        </CardHeader>
+        <CardBody>
+          <Text>
+            With Chakra UI, I wanted to sync the speed of development with the
+            speed of design. I wanted the developer to be just as excited as the
+            designer to create a screen.
+          </Text>
+        </CardBody>
+        <Image
+          objectFit="cover"
+          src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          alt="Chakra UI"
+        />
+
+        <CardFooter
+          justify="space-between"
+          flexWrap="wrap"
+          sx={{
+            "& > button": {
+              minW: "136px",
+            },
+          }}
+        >
+          <Button flex="1" variant="ghost">
+            Like
+          </Button>
+          <Button flex="1" variant="ghost">
+            Comment
+          </Button>
+          <Button flex="1" variant="ghost">
+            Share
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
