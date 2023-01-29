64ac592901f02eef649743dfca15e5607f70a8ed
Jorge Mancilla <jorgemancilla10109@hotmail.com>
2023-01-29 05:37:04 -0600

    add stuff to front end and fix junk

--- packages/frontend/src/components/SimpleModal.tsx
+++ packages/frontend/src/components/SimpleModal.tsx
@@ -1,242 +1,172 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Stack,
-  StackDivider,
-  useDisclosure,
-  Text,
-  Spinner,
-  Flex,
-  Spacer,
-  Center,
-  ButtonGroup,
-  IconButton,
-  Tag,
-  Avatar,
-  CardFooter,
-  Image,
-  Container
-} from "@chakra-ui/react";
-import { stringify } from "querystring";
-import React, { useEffect, useState } from "react";
-import { HiOutlineClipboard } from "react-icons/hi";
-import { LegislatorCard } from "./LegislatorCard";
+import { Box, Button, Card, CardBody, CardHeader, FormControl, FormLabel, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, StackDivider, useDisclosure, Text, Spinner, Flex, Spacer, Center, ButtonGroup, IconButton, Tag, Badge } from "@chakra-ui/react"
+import { stringify } from "querystring"
+import React, { useEffect, useState } from "react"
+import { HiOutlineClipboard } from "react-icons/hi"
 
 export function SimpleModal() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
-  const [loading1, setLoading1] = useState(false);
-  const [loading2, setLoading2] = useState(false);
+    const [loading1, setLoading1] = useState(false)
+    const [loading2, setLoading2] = useState(false)
 
     const [summary, setSummary] = useState(" ")
     const [proposal, setProposal] = useState(" ")
     const [classification, setClassification] = useState<{label: string, percentage: string}>({label: " ", percentage: " " })
+    const [personInfo, setPersonInfo] = useState<any>({})
   
     const initialRef = React.useRef(null)
     const finalRef = React.useRef(null)
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onClose();
+    const handleSubmit = event => {
+      event.preventDefault();
+      onClose()
 
       setSummary("")
       setProposal("")
       setClassification({label: "", percentage: ""})
+      setPersonInfo({})
 
-    const message =
-      "a bill proposal that addresses " +
-      question1 +
-      ". The intended outcome would be " +
-      question2;
+      const message = "a bill proposal that addresses " + question1 + ". The intended outcome would be " + question2
 
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ message: message.toLowerCase() }),
-    };
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: message.toLowerCase() })
+      };
+      
+      setLoading1(true)
+      setLoading2(true)
 
-    setLoading1(true);
-    setLoading2(true);
+      fetch("api/BillDirection", requestOptions)
+        .then((response) => response.json())
+        .then((data) => setSummary(data[0].generated_text))
+        .catch((err) => {
+          alert("BillDirection error: " + err)
+        })
+        .finally(() => setLoading1(false))
 
-    fetch("api/BillDirection", requestOptions)
-      .then((response) => response.json())
-      .then((data) => setSummary(data[0].generated_text))
-      .catch((err) => {
-        alert("BillDirection error: " + err);
-      })
-      .finally(() => setLoading1(false));
+      fetch("api/getProposal", requestOptions)
+        .then((response) => response.json())
+        .then((data) => setProposal(data))
+        .catch((err) => {
+          alert("getProposal error: " + err)
+        })
+        .finally(() => setLoading2(false))
 
-    fetch("api/getProposal", requestOptions)
-      .then((response) => response.json())
-      .then((data) => setProposal(data))
-      .catch((err) => {
-        alert("getProposal error: " + err);
-      })
-      .finally(() => setLoading2(false));
+      fetch("api/billCassification", requestOptions)
+        .then((response) => response.json())
+        .then((data) => setClassification({label: data[0].label, percentage: Number(data[0].score).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2})}))
+        .catch((err) => {
+          alert("billClassification error: " + err)
+        })
 
-    fetch("api/billCassification", requestOptions)
-      .then((response) => response.json())
-      .then((data) =>
-        setClassification({
-          label: data[0].label,
-          percentage: Number(data[0].score).toLocaleString(undefined, {
-            style: "percent",
-            minimumFractionDigits: 2,
-          }),
+      fetch("api/getReleventPersonInfo", requestOptions)
+        .then((response) => response.json())
+        .then((data) => setPersonInfo(data))
+        .catch((err) => {
+          alert("getReleventPersonInfo error: " + err)
         })
         
     };
 
-  const [question1, setQuestion1] = useState("");
-  const [question2, setQuestion2] = useState("");
+    const [question1, setQuestion1] = useState('');
+    const [question2, setQuestion2] = useState('');
 
-  return (
-    <>
-      <Button onClick={onOpen} color="green.400" margin={5}>
-        Generate
-      </Button>
+    return (
+      <>
+        <Button onClick={onOpen} color="green.400" margin={5}>Generate</Button>
+  
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>First, answer some questions</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl isRequired>
+                <FormLabel>What is the problem that you want to address?</FormLabel>
+                <Input ref={initialRef} placeholder='question1' 
+                onChange={event => setQuestion1(event.currentTarget.value)}/>
+              </FormControl>
+  
+              <FormControl mt={4} isRequired>
+                <FormLabel>What outcomes do you want to see?</FormLabel>
+                <Input placeholder='question2' 
+                onChange={event => setQuestion2(event.currentTarget.value)}/>
+              </FormControl>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={handleSubmit}>
+                Generate
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
 
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>First, answer some questions</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl isRequired>
-              <FormLabel>
-                What is the problem that you want to address?
-              </FormLabel>
-              <Input
-                ref={initialRef}
-                placeholder="question1"
-                onChange={(event) => setQuestion1(event.currentTarget.value)}
-              />
-            </FormControl>
-
-            <FormControl mt={4} isRequired>
-              <FormLabel>What outcomes do you want to see?</FormLabel>
-              <Input
-                placeholder="question2"
-                onChange={(event) => setQuestion2(event.currentTarget.value)}
-              />
-            </FormControl>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
-              Generate
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-      <Flex wrap={"wrap"} gridGap={5}>
-        <Card width={600} height={"auto"}>
-          {proposal ? (
-            <CardBody>
-              <Stack divider={<StackDivider />} spacing="4">
+        <Flex wrap={"wrap"} gridGap={5}>
+          <Card width={600} height={"auto"}>
+          {proposal ? <CardBody>
+              <Stack divider={<StackDivider />} spacing='4'>
                 <Box>
                   <ButtonGroup>
-                    <Heading size="sm" textTransform="uppercase">
+                    <Heading size='sm' textTransform='uppercase'>
                       Bill Proposal
                     </Heading>
-                    <IconButton
-                      size={"xs"}
-                      variant={"outline"}
-                      icon={<HiOutlineClipboard />}
-                      aria-label="copy"
-                    />
+                    <IconButton size={"xs"} variant={"outline"} icon={<HiOutlineClipboard/>} aria-label="copy"/> 
                   </ButtonGroup>
-
-                  <Text pt="2" fontSize="sm" whiteSpace={"pre-wrap"}>
+                       
+                  <Text pt='2' fontSize='sm' whiteSpace={"pre-wrap"}>
                     {proposal.toString()}
                   </Text>
                 </Box>
               </Stack>
-            </CardBody>
-          ) : (
-            <>
-              <Spinner
-                thickness="4px"
-                speed="0.65s"
-                emptyColor="gray.200"
-                color="blue.500"
-                size="xl"
+            </CardBody> : <><Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
                 alignSelf={"auto"}
-              />
-              <Text>Proposal Loading...</Text>
-            </>
-          )}
-        </Card>
-        <Card width={600} height={200}>
-          {summary ? (
-            <CardBody>
-              <Stack divider={<StackDivider />} spacing="4">
+              /><Text>Proposal Loading...</Text></>}
+          </Card>
+          <Card width={600} height={200}>
+            {summary ? <CardBody>
+              <Stack divider={<StackDivider />} spacing='4'>
                 <Box>
-                  <Heading size="xs" textTransform="uppercase">
+                  <Heading size='xs' textTransform='uppercase'>
                     Summary
                   </Heading>
-                  <Text pt="2" fontSize="sm" whiteSpace={"pre-wrap"}>
+                  <Text pt='2' fontSize='sm' whiteSpace={"pre-wrap"}>
                     {summary.toString()}
                   </Text>
                 </Box>
               </Stack>
-            </CardBody>
-          ) : (
-            <>
-              <Spinner
-                thickness="4px"
-                speed="0.65s"
-                emptyColor="gray.200"
-                color="blue.500"
-                size="xl"
-                alignSelf={"auto"}
-              />
-              <Text>Summary Loading...</Text>
-            </>
-          )}
-        </Card>
+            </CardBody> : <><Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+            alignSelf={"auto"}
+          /><Text>Summary Loading...</Text></>}
+          </Card>
 
-        {classification.label ? (
-          <Tag height={10}>
-            Classification:{" "}
-            {" " + classification.label + " " + classification.percentage}
-          </Tag>
-        ) : (
-          <>
-            <Spinner
-              thickness="4px"
-              speed="0.65s"
-              emptyColor="gray.200"
-              color="blue.500"
-              size="xl"
-              alignSelf={"auto"}
-            />
-            <Text>Classification Loading...</Text>
-          </>
-        )}
+          {classification.label ? <Badge colorScheme={'green'} height={10}>{classification.label + " " + classification.percentage}</Badge>: <><Spinner
+                          thickness='4px'
+                          speed='0.65s'
+                          emptyColor='gray.200'
+                          color='blue.500'
+                          size='xl'
+                          alignSelf={"auto"}
+                        /><Text>Classification Loading...</Text></>}
 
-        {/* <Card width={600} height={200}>
+          {/* <Card width={600} height={200}>
             {summary ? <CardBody>
               <Stack divider={<StackDivider />} spacing='4'>
                 <Box>
@@ -257,14 +187,8 @@ export function SimpleModal() {
             alignSelf={"auto"}
           /><Text>Summary Loading...</Text></>}
           </Card> */}
-        <Container>
-          <Stack>
-            <LegislatorCard />
-          </Stack>
-        </Container>
 
-        
-      </Flex>
-    </>
-  );
-}
+        </Flex>
+      </>
+    )
+  }
