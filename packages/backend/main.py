966a67e4cf88e822e597418487eebbf84958de39
joe <joseph.white.bsa@gmail.com>
2023-01-29 01:03:03 -0600

    added ai purposal creation to main

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -3,6 +3,11 @@ from transformers import pipeline
 import tensorflow as tf
 import asyncio
 from pydantic import BaseModel
+from datetime import date
+import os
+import openai
+from dotenv import load_dotenv
+from pathlib import Path
 
 app = FastAPI()
 
@@ -18,6 +23,11 @@ app = FastAPI()
 
 class Data(BaseModel):
     message: str
+
+
+dotenv_path = Path('..\MinneHack-2023\.env')
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.get("/")
 async def root():
@@ -59,7 +69,7 @@ async def billCassification(message):
 
 @app.post("/BillDirection")
 async def BillDirection(data: Data):
-    """
+    """s
     takes in a prompt then provides direction of where to go to get this bill passed
 
     Model type: PyTorch
@@ -71,11 +81,71 @@ async def BillDirection(data: Data):
     
     return generator(data.message)
 
+
+@app.post("/getProposal")
+async def getProposal(prompt):
+    today = date.today()
+    intro = f"""
+    {today}\n
+    (officials name)\n
+    (officials title)\n
+    (office/commitee title)\n
+    (officials address)\n
+    (officials city, state and zip code)\n\n
+    """
+
+    title = getTitle(prompt)
+
+    body = getBody(prompt)
+
+    ethank = """I am looking forward to hearing your input on this and am hoping you consider my input.\n
+                Thank you for your time and consideration.\n\n
+                Sincerely,\n\n
+                \n\n\n
+                (your name)"""
+    
+    return (intro + title[0]['generated_text'] + "\n" + body + "\n" + ethank)
+
+
+def getTitle(message):
+    """
+    takes in a prompt then provides a title based on the scentence
+
+    Model type: PyTorch
+
+    """
+
+    generator = pipeline(
+        "text2text-generation", model="czearing/article-title-generator")
+    
+    return generator(message)
+
+
+
+def getBody(prompt):
+
+    gpt_prompt = f"""Create me a proposal for a legislative bill. This is a {prompt}
+    Structure it in a 4 paragraph format. First paragraph states why it the topic is imporant. The second paragraph will provide relevant
+    statistics on the topic. The third paragraph will talk about how the bill will be benifitial in any way. The fourth paragrph should
+    wrap up this letter, and ask for a pertnership or the abulity to work together with this person in the future to fix this issue.     
+    """
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=gpt_prompt,
+        temperature=1.0, #A low temperature (near 0) is going to give very well-defined answers consistently while a higher number (near 1) will be more creative in its responses
+        max_tokens=750,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    #
+    return(response['choices'][0]['text'])
+
 # for testing
-msg = "a bill proposal that addresses an invasive species in Minnesota. The intended outcome would be to eliminate or reduce the population of the invasive species"
-smtst = "a bill proposal that addresses an invasive species in Minnesota"
-output = asyncio.run(summerizePY(smtst))
-print(output)
+# msg = "a bill proposal that addresses an invasive species in Minnesota. The intended outcome would be to eliminate or reduce the population of the invasive species"
+# smtst = "a bill proposal that addresses an invasive species in Minnesota"
+# output = asyncio.run(summerizePY(smtst))
+# print(output)
 
 
 
--- packages/backend/proposalGeneration.py
+++ packages/backend/proposalGeneration.py
@@ -17,9 +17,8 @@ today = date.today()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def getProposal(prompt):
-    
-
-    intro = """
+   
+    intro = f"""
     {today}\n
     (officials name)\n
     (officials title)\n
@@ -28,18 +27,20 @@ def getProposal(prompt):
     (officials city, state and zip code)\n\n
     """
 
-    title = str(getTitle(prompt))
+    title = getTitle(prompt)
 
-    body = str(getBody(prompt))
+    body = getBody(prompt)
 
     ethank = """I am looking forward to hearing your input on this and am hoping you consider my input.\n
                 Thank you for your time and consideration.\n\n
                 Sincerely,\n\n
                 \n\n\n
                 (your name)"""
+    
+    return (intro + title[0]['generated_text'] + "\n" + body + "\n" + ethank)
 
 
-async def getTitle(message):
+def getTitle(message):
     """
     takes in a prompt then provides a title based on the scentence
 
@@ -56,21 +57,22 @@ async def getTitle(message):
 
 def getBody(prompt):
 
-    gpt_prompt = """Create me a proposal for a legislative bill. This is a {prompt} \n
+    gpt_prompt = f"""Create me a proposal for a legislative bill. This is a {prompt}
     Structure it in a 4 paragraph format. First paragraph states why it the topic is imporant. The second paragraph will provide relevant
     statistics on the topic. The third paragraph will talk about how the bill will be benifitial in any way. The fourth paragrph should
     wrap up this letter, and ask for a pertnership or the abulity to work together with this person in the future to fix this issue.     
     """
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=prompt,
+        prompt=gpt_prompt,
         temperature=1.0, #A low temperature (near 0) is going to give very well-defined answers consistently while a higher number (near 1) will be more creative in its responses
         max_tokens=750,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0
     )
-    print(response['choices'][0]['text'])
+    #
+    return(response['choices'][0]['text'])
 
 
 def getModels():
@@ -81,4 +83,4 @@ def getModels():
 
 msg = "a bill proposal that addresses an invasive species in Minnesota. The intended outcome would be to eliminate or reduce the population of the invasive species"
 
-getProposal(msg)
+print(getProposal(msg))
